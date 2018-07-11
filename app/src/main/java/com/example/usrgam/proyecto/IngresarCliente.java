bdef62d48b59e9393dc615539c455e09fd151fbc
Ricardo Ortiz <ricardoo1995@hotmail.com>
2018-07-11 15:56:48 -0500

    Creación Proyecto

---
+++ app/src/main/java/com/example/usrgam/proyecto/IngresarCliente.java
@@ -0,0 +1,13 @@
+package com.example.usrgam.proyecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IngresarCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingresar_cliente);
+    }
+}
